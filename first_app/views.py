8338d3fe8bb7e8956a5c5bb1b632822e9dd873b8
Anatoli <PanasAA@web.de>
2025-03-31 14:13:47 +0200

    add proj

---
+++ first_app/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def django_greetings(request) -> HttpResponse:
+    name = "Anatoli"
+    return HttpResponse(
+        "<h1>Greeting from the Django APP!!! :)</h1>"
+    )
+
+def user_greetings(request, name = "Anatoli"):
+    # name = "Anatoli"
+    return HttpResponse(
+        f"<h1>Greeting, {name}!!! :)</h1>"
+    )
+
+def index_page(request):
+    return render(request, 'my_html.html')
