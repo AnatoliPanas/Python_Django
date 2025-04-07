f6a04888be509602a90a29a70fbd4b99603e3ddf
Anatoli <PanasAA@web.de>
2025-04-07 10:08:56 +0200

    add proj

--- books/models.py
+++ books/models.py
@@ -7,13 +7,19 @@ class AuthorBio(models.Model):
     created_att = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        if self.author:
+            return f"{self.author.name[0]}. {self.author.surname}'s BIO"
+        return "No Author"
+
 class Author(models.Model):
     name = models.CharField(max_length=30)
     surname = models.CharField(max_length=30)
     bio = models.OneToOneField(
         AuthorBio,
         on_delete=models.PROTECT,
-        null=True
+        null=True,
+        related_name="author"
     )
 
     def __str__(self):
@@ -34,10 +40,20 @@ class Book(models.Model):
     rating = models.FloatField(default=0.0)
     genre = models.CharField(max_length=30, choices=GENRE_CHOICES)
     release_year = models.DateField()
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
+    author = models.ForeignKey(Author,
+                               on_delete=models.CASCADE,
+                               null=True,
+                               related_name="book")
     pages = models.SmallIntegerField(null=True, blank=True)
     language = models.CharField(max_length=15, default="English")
     isbn = models.CharField(max_length=50)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = "Book"
+        verbose_name_plural = "Books"
+        unique_together = ("title", "author")
+        ordering = ("release_year")
+
